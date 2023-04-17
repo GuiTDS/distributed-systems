a74c221bf68d496ce8ab70b9fd1c34c9142a0e3f
Guilherme Sanches <guitdsanches@gmail.com>
2023-04-17 12:15:35 -0300

    adding the connection class and methods to sign up and authenticate users in the DB

---
+++ src/control/ConexaoControl.java
@@ -0,0 +1,25 @@
+package control;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.*;
+
+public class ConexaoControl {
+	 //public Statement stm; // Responsavel por preparar e realizar pesquisas no banco de dados;
+	 //public ResultSet rs; // Responsavel por armazenar o resultado de um pesquisa passada para o statement;
+	 private String caminho = "jdbc:postgresql:sistemas_distribuidos"; // O "meubanco" representa a minha database 
+	 private String usuario = "postgres"; // Usuario default
+	 private String senha = "gui3665";
+	 public Connection conn; // Responsavel por realizar a conexão com o banco de dados;
+	    
+	public Connection conectaBD() {
+		
+		try {
+            conn = DriverManager.getConnection(caminho, usuario, senha); // Realiza a conexão com o banco;
+		} catch(SQLException erro) {
+			erro.printStackTrace();
+		}
+		return conn;
+	}
+}
--- src/control/UserControl.java
+++ src/control/UserControl.java
@@ -1,15 +1,67 @@
 package control;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.swing.JOptionPane;
+
 import model.SignUpValidator;
 import model.User;
 
+
+
 public class UserControl {
+	Connection conn;
+	PreparedStatement pstm;
 	
-	public boolean signUpUser(User user) {
+	public boolean signUpUser(User user) { // metodo de cadastro para teste
 		SignUpValidator validator = new SignUpValidator(user);
 		if(validator.isValid()) {
 			System.out.println("Usuario valido...realizando cadastro no banco de dados!");
 			return true;
+		}
+		return false;
+	}
+	
+	public boolean cadastrarUsuario(User user) { // esse será o metodo de cadastrar
+		conn = new ConexaoControl().conectaBD();
+		try {
+			String sql = "INSERT INTO users (name, email, password) VALUES (?, ?, ?);";
+			pstm = conn.prepareStatement(sql);	
+			pstm.setString(1, user.getName());
+			pstm.setString(2, user.getEmail());
+			pstm.setString(3, user.getPassword());
+			
+			pstm.execute();
+			pstm.close();
+			return true;
+		} catch (SQLException erro) {
+			JOptionPane.showMessageDialog(null, "Usuario control: " + erro);
+			return false;
+		}
+	}
+	
+	public boolean authenticateUser(User user) {
+		conn = new ConexaoControl().conectaBD();
+		
+		try {
+			String sql = "SELECT * FROM users WHERE email = ? AND password = ?;";
+			pstm = conn.prepareStatement(sql);
+			pstm.setString(1, user.getEmail());
+			pstm.setString(2, user.getPassword());
+			
+			ResultSet result = pstm.executeQuery();
+			
+			if(result.next()) {
+				return true;
+			}
+			
+			
+		} catch (SQLException erro) {
+			JOptionPane.showMessageDialog(null, "Usuario control: " + erro);
+			return false;
 		}
 		return false;
 	}
